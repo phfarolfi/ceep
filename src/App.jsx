ecd6f3367d660d42b761151ebdacb49f90fddce1
Pedro Farolfi <phfarolfi@gmail.com>
2021-11-26 19:01:10 -0300

    Applying styles to React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ListaDeNotas from './components/ListaDeNotas'
 import FormularioCadastro from './components/FormularioCadastro'
 import { Component } from 'react';
+import './assets/index.css'
 
 class App extends Component {
     render () {
