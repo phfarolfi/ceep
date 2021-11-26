c97655a09182ade314bb7f2788e189ec6d534688
Pedro Farolfi <phfarolfi@gmail.com>
2021-11-26 19:04:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ class App extends Component {
     render () {
         return (
             <section>
-            <FormularioCadastro/>
-            <ListaDeNotas/>
-        </section>
+                <FormularioCadastro/>
+                <ListaDeNotas/>
+            </section>
         );
     }
 }
