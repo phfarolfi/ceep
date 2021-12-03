b838c63286732d51116c69e2e83ea443fe8645d4
Pedro H. Farolfi <pedro.farolfi@intelie.com.br>
2021-12-03 18:19:02 -0300

    ceep update

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.notas = []
+        this.state = {}
+    }
+
+    criarNota(titulo, texto) {
+        const novaNota = {titulo, texto}
+        this.notas.push(novaNota)
+        this.setState({
+            notas: this.notas
+        })
+    }
+
+    render() {
+        return (
+            <section className="conteudo">
+                <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+                <ListaDeNotas notas={this.notas}/>
+            </section>
+        );
+    }
+}
+
+export default App;
